26b65bf430d546162137bb0b0129b61badbde4fd
Brian Curless <bcurl3ss@gmail.com>
2024-04-22 14:14:10 -0700

    Made minor fixes to ReLULayer
    Made minor changes to Eli's initial implementation of ReLU layer. Mostly
    just compilation issues that I found with my language server. Lots of
    auto formatting changes showed up too.

--- src/ReLULayer.cu
+++ src/ReLULayer.cu
@@ -2,69 +2,63 @@
 #include "cudaHelpers.cuh"
 #include "math.h"
 
-__global__ void reluForward(reluInput_t *inputs);
-__global__ void reluBackward(float *upstreamGradients, reluInput_t *inputs, float *gradientsOut);
+__global__ void reluForwardKernel(reluInput_t inputs);
+__global__ void reluBackwardKernel(float *upstreamGradients, reluInput_t inputs,
+                                   float *gradientsOut);
 
-void reluForward(reluInput_t *inputs)
-{
-	// Set params for our kernel
+void reluForward(reluInput_t *inputs) {
+    // Set params for our kernel
     int blockDim = 128;
-    int gridDim = ceil(1.0 * inputs->Dim / blockDim);
+    int gridDim = ceil(1.0 * inputs->dim / blockDim);
 
     // Run cuda kernel for forward relu operation
-    reluForward<<<gridDim, blockDim>>>(inputs);
+    reluForwardKernel<<<gridDim, blockDim>>>(*inputs);
     gpuErrchk(cudaDeviceSynchronize());
 }
 
-void reluBackward(float *upstreamGradients, reluInput_t *inputs, float *gradientsOut)
-{
-	// Set params for our kernel
+void reluBackward(float *upstreamGradients, reluInput_t *inputs, float *gradientsOut) {
+    // Set params for our kernel
     int blockDim = 128;
-    int gridDim = ceil(1.0 * inputs->Dim / blockDim);
+    int gridDim = ceil(1.0 * inputs->dim / blockDim);
 
-	// Run cuda kernel for backward relu operation
-    reluBackward<<<gridDim, blockDim>>>(upstreamGradients, inputs, gradientsOut);
+    // Run cuda kernel for backward relu operation
+    reluBackwardKernel<<<gridDim, blockDim>>>(upstreamGradients, *inputs, gradientsOut);
     gpuErrchk(cudaDeviceSynchronize());
 }
 
-__global__ void reluForward(reluInput_t *inputs) 
-{
-	// Assign tid
-	int tid = threadIdx.x + blockDim.x * blockIdx.x;
+__global__ void reluForwardKernel(reluInput_t inputs) {
+    // Assign tid
+    int tid = threadIdx.x + blockDim.x * blockIdx.x;
 
     // Make sure thread is not out of bounds for our dim
-	if( tid < inputs->dim) 
-	{
-		// Set thread's output to 0 automatically
-		inputs->outputs[index] = 0;
+    if (tid < inputs.dim) {
+        // Set thread's output to 0 automatically
+        inputs.outputs[tid] = 0;
 
-		// Reset output to whatever is bigger: 0 or our input
-		AtomicMax(inputs->outputs[index], inputs->inputs[index]);
-	}
+        // Reset output to whatever is bigger: 0 or our input
+        atomicMax(&(inputs.outputs[tid]), inputs.inputs[tid]);
+    }
 
-	// Operation done, end kernel
+    // Operation done, end kernel
 }
 
-__global__ void reluBackward(float *upstreamGradients, reluInput_t *inputs, float *gradientsOut) 
-{
-	// Assign tid
-	int tid = threadIdx.x + blockDim.x * blockIdx.x;
+__global__ void reluBackwardKernel(float *upstreamGradients, reluInput_t inputs,
+                                   float *gradientsOut) {
+    // Assign tid
+    int tid = threadIdx.x + blockDim.x * blockIdx.x;
 
     // Make sure thread isn't out of bounds for our dim
-	if( tid < inputs->dim )
-	{
-		// Set gradientsOut to 0 automatically
-	   *gradientsOut[index] = 0;
+    if (tid < inputs.dim) {
+        // Set gradientsOut to 0 automatically
+        gradientsOut[tid] = 0;
 
-       // Check if our input is greater than 0
-	   if( inputs->input[index] > 0)
-	   {
-		   // Reset gradientsOut for our index to whatever our upstreamGradients is
-		   // (1 * upstreamGradients) = upstreamGradients
-		   atomicAdd(&gradientsOut[index], *upstreamGradients);
-	   }
-	}
+        // Check if our input is greater than 0
+        if (inputs.inputs[tid] > 0) {
+            // Reset gradientsOut for our index to whatever our upstreamGradients is
+            // (1 * upstreamGradients) = upstreamGradients
+            atomicAdd(&gradientsOut[tid], upstreamGradients[tid]);
+        }
+    }
 
-	// Operation done, end kernel
+    // Operation done, end kernel
 }
-
